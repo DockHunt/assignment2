a5e11620d65619f3f4c7da61afe790b8e6081304
Dock Hunter <hlh0031@auburn.edu>
2019-08-27 15:02:43 -0500

    2nd commit

--- src/test/java/FinderTest.java
+++ src/test/java/FinderTest.java
@@ -1,43 +1,43 @@
 import org.junit.jupiter.api.Test;
-import package.Finder;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FinderTest {
 
+    Finder testFinder = new Finder();
     @Test
     public void findMaxTestvalid() throws Exception {
         int array[] = {1, 5, -2, 2};
-        assertEquals(5, findMax(array));
+        assertEquals(5, testFinder.findMax(array));
     }
 
     @Test
     public void findMaxTestnull() throws Exception {
         int array[] = null;
-        assertEquals(null, findMax(array));
+        assertEquals(null, testFinder.findMax(array));
     }
 
     @Test
     public void findMaxTestempty() throws Exception {
         int array[] = {};
-        assertEquals(null, findMax(array));
+        assertEquals(null, testFinder.findMax(array));
     }
 
     @Test
     public void findMinTestvalid() throws Exception {
         int array[] = {1, 5, -2, 2};
-        assertEquals(-2, findMin(array));
+        assertEquals(-2, testFinder.findMin(array));
     }
 
     @Test
     public void findMinTestnull() throws Exception {
         int array[] = null;
-        assertEquals(null, findMin(array));
+        assertEquals(null, testFinder.findMin(array));
     }
 
     @Test
     public void findMinTestempty() throws Exception {
         int array[] = {};
-        assertEquals(null, findMin(array));
+        assertEquals(null, testFinder.findMin(array));
     }
 }
